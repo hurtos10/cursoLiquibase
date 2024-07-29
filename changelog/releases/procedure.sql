3c0c3b0d73b3b18e582d17a7a0587aa0b366e2e8
MiguelBautista3011 <Miguel.Bautista@axity.com>
2024-07-29 17:15:12 -0600

    Revert "añadidos archivos de ejercicio"
    This reverts commit 5d5b36f2fa6ac0c15dc6052906f8417c5bf591cd.

--- changelog/releases/procedure.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE createExamplePerson()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO personas (nombre, ap_paterno, ap_materno, edad)
-    VALUES ('Nombre Generico', 'AP_Paterno Generico', 'AP_Materno Generico', 1);
-END;
-$$;
